ce1e5514e1f3e48159bd7fa4128d987f1bf55da4
finskiyzaliv <elsukovaleksandr55@gmail.com>
2024-04-10 11:42:01 +0300

    Initial commit

---
+++ src/main/java/app/App.java
@@ -0,0 +1,15 @@
+package app;
+
+import bank.ATM;
+import bank.Bank;
+import bank.Card;
+import data.BankAccount;
+
+import java.util.List;
+
+public class App {
+    public static void main(String[] args){
+        ATM atm = new ATM();
+        atm.start();
+    }
+}
